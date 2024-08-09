90d33db7f5c8c4eb0019694521b71ea5a26a7426
Sambhav Dave <sambhavd4@gmail.com>
2024-08-09 22:59:58 +0530

    Modifier page objects

--- src/pages/login_page.py
+++ src/pages/login_page.py
@@ -7,9 +7,17 @@ class LoginPage:
 
     def __init__(self, page: Page):
         self.page = page
+        self.flash_incorrect_cred = page.locator("//div[@id='flash']/b[contains(.,'invalid')]")
 
     def login(self, username: str, password: str):
         lm = login_modal.LoginModal(self.page)
         lm.enter_username(username)
         lm.enter_password(password)
-        # loginModal.click_login_btn()
+        lm.click_login_btn()
+
+    def is_incorrect_credential_flash_displayed(self) -> bool:
+        try:
+            expect(self.flash_incorrect_cred).to_be_visible(timeout=10000)  # 10 seconds timeout
+            return True
+        except AssertionError:
+            return False
