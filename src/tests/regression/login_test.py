457321a6d7daea5a044ad55b0c15ee86488c369a
Sambhav Dave <sambhavd4@gmail.com>
2024-08-09 23:00:19 +0530

    Refactored and added more test methods

--- src/tests/regression/login_test.py
+++ src/tests/regression/login_test.py
@@ -2,8 +2,13 @@ from playwright.sync_api import expect
 from src.pages.login_page import LoginPage
 
 
-def test_login(page):
-    page.goto("https://practice.expandtesting.com/login")
+def test_login_title(page, login_page_url):
+    page.goto(login_page_url)
+    expect(page).to_have_title("Test Login form page for Automation Testing Practice")
+
+
+def test_successful_login(page, login_page_url):
+    page.goto(login_page_url)
     lp = LoginPage(page)
 
     username = "practice"
@@ -11,4 +16,16 @@ def test_login(page):
 
     lp.login(username, password)
 
-    expect(page).to_have_url("https://practice.expandtesting.com/dashboard")
+    expect(page).not_to_have_url(login_page_url)
+
+
+def test_unsuccessful_login(page, login_page_url):
+    page.goto(login_page_url)
+    lp = LoginPage(page)
+
+    username = "sam"
+    password = "legend"
+
+    lp.login(username, password)
+
+    assert lp.is_incorrect_credential_flash_displayed() is True
