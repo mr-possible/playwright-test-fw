d9164bdffdf209a0b13618dfdefa566baa6f6355
Sambhav Dave <sambhavd4@gmail.com>
2024-08-09 22:59:47 +0530

    Added base test

--- src/tests/conftest.py
+++ src/tests/conftest.py
@@ -1,13 +1,27 @@
-import pytest
+import os, pytest
+from dotenv import load_dotenv
+
 from playwright.sync_api import sync_playwright
+
+load_dotenv()
+
+
+@pytest.fixture(scope="session")
+def config():
+    return config
 
 
 @pytest.fixture(scope="session")
 def browser():
     with sync_playwright() as p:
-        browser = p.chromium.launch(headless=False)
+        browser = p.chromium.launch(headless=True)
         yield browser
         browser.close()
+
+
+@pytest.fixture(scope="session")
+def login_page_url(config):
+    return os.environ.get('login_url')
 
 
 @pytest.fixture(scope="function")
